bf92455460b779402445e655ebc7c83fad391c29
Menath Nadungoda <nadungmenath1@gmail.com>
2024-06-04 00:41:29 +0530

    changed the goToTop button function for more readable

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
-
     $(window).scroll(function(){
-        if($(this).scrollTop() > 400){
-           $('#topBtn').fadeIn();
-        } else{
+        if ($(this).scrollTop() > 100) {
+            $('#topBtn').fadeIn();
+        } else {
             $('#topBtn').fadeOut();
         }
     });
-
-    $("#topBtn").click(function(){
-        $('html, body').animate({scrollTop : 0}, 800);
+    $('#topBtn').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
     });
 });
